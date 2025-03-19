25cc93245c9cb9bb581a8cc0816b574ae889d4ec
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2025-03-19 15:46:48 +0100

    1

--- Class1/TestProject/UnitTest1.cs
+++ Class1/TestProject/UnitTest1.cs
@@ -14,33 +14,108 @@ namespace TestProjectPhone
             var telefon = new Phone(wlasciciel, numer);
 
             Assert.AreEqual(wlasciciel, telefon.Owner);
+            Assert.AreEqual(numer, telefon.PhoneNumber);
+            Assert.AreEqual(0, telefon.Count);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void Konstruktor_ZaKrotiNumerTelefonu_ArgumentException()
+        public void Konstruktor_ZaKrotkiNumerTelefonu_ArgumentException()
         {
             var wlasciciel = "Jan Kowalski";
             var numer = "12345678";
 
             var telefon = new Phone(wlasciciel, numer);
-
-            //Assert.ThrowsException<ArgumentException>(() => new Phone(wlasciciel, numer));
-
-            //try
-            //{
-            //    var temp = new Phone(wlasciciel, numer);
-            //}
-            //catch (ArgumentException e)
-            //{
-            //    Assert.ThrowsException<ArgumentException>(() => throw e);
-            //    return;
-            //}
         }
-    }
 
-    class Pomocnicza
-    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Konstruktor_NullOwner_ArgumentException()
+        {
+            var numer = "123456789";
 
+            var telefon = new Phone(null, numer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Konstruktor_EmptyOwner_ArgumentException()
+        {
+            var numer = "123456789";
+
+            var telefon = new Phone(string.Empty, numer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Konstruktor_InvalidPhoneNumber_ArgumentException()
+        {
+            var wlasciciel = "Jan Kowalski";
+            var numer = "12345678a";
+
+            var telefon = new Phone(wlasciciel, numer);
+        }
+
+        [TestMethod]
+        public void AddContact_ValidContact_ShouldAddSuccessfully()
+        {
+            var telefon = new Phone("Jan Kowalski", "123456789");
+            var contactName = "Anna Nowak";
+            var contactNumber = "987654321";
+
+            var result = telefon.AddContact(contactName, contactNumber);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(1, telefon.Count);
+        }
+
+        [TestMethod]
+        public void AddContact_DuplicateContact_ShouldReturnFalse()
+        {
+            var telefon = new Phone("Jan Kowalski", "123456789");
+            var contactName = "Anna Nowak";
+            var contactNumber = "987654321";
+            telefon.AddContact(contactName, contactNumber);
+
+            var result = telefon.AddContact(contactName, contactNumber);
+
+            Assert.IsFalse(result);
+            Assert.AreEqual(1, telefon.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AddContact_PhoneBookFull_ShouldThrowInvalidOperationException()
+        {
+            var telefon = new Phone("Jan Kowalski", "123456789");
+            for (int i = 0; i < telefon.PhoneBookCapacity; i++)
+            {
+                telefon.AddContact($"Contact{i}", $"12345678{i % 10}");
+            }
+
+            telefon.AddContact("New Contact", "987654321");
+        }
+
+        [TestMethod]
+        public void Call_ExistingContact_ShouldReturnCallingMessage()
+        {
+            var telefon = new Phone("Jan Kowalski", "123456789");
+            var contactName = "Anna Nowak";
+            var contactNumber = "987654321";
+            telefon.AddContact(contactName, contactNumber);
+
+            var result = telefon.Call(contactName);
+
+            Assert.AreEqual($"Calling {contactNumber} ({contactName}) ...", result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Call_NonExistingContact_ShouldThrowInvalidOperationException()
+        {
+            var telefon = new Phone("Jan Kowalski", "123456789");
+
+            telefon.Call("NonExistingContact");
+        }
     }
 }
